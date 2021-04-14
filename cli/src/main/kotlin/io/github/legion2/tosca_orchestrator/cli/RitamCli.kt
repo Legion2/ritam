3c35c0e92292227a748143b5eb85490ce94142c1
Leon Kiefer <leon.k97@gmx.de>
2021-04-14 12:12:48 +0200

    version 1.0.0

--- cli/src/main/kotlin/io/github/legion2/tosca_orchestrator/cli/RitamCli.kt
+++ cli/src/main/kotlin/io/github/legion2/tosca_orchestrator/cli/RitamCli.kt
@@ -16,7 +16,7 @@ class RitamCli : CliktCommand(name = "ritam", autoCompleteEnvvar = "RITAM_CLI_CO
     ).default("http://localhost:8080")
 
     init {
-        versionOption("0.5.0")
+        versionOption("1.0.0")
         context {
             autoEnvvarPrefix = "RITAM_CLI"
         }
